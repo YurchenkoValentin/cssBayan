6e91a3432bf83e9b49a130ba78d0c17d004ca031
Valentin <valentin@yurchenko.com>
2023-03-13 03:41:37 +0800

    feat: add images (Sun, March 12, 2023 22:41 PM)

---
+++ cssBayan/script.js
@@ -0,0 +1,14 @@
+const accordionItems = document.querySelectorAll('.accordion-item');
+
+accordionItems.forEach(item => {
+  const header = item.querySelector('.accordion-header');
+  const content = item.querySelector('.accordion-content');
+
+  header.addEventListener('click', () => {
+    if (content.style.display === 'block') {
+      content.style.display = 'none';
+    } else {
+      content.style.display = 'block';
+    }
+  });
+});
